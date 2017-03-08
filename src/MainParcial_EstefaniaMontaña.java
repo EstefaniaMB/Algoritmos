c9e494bd4ae2b0ea52268d1f874d3ece076e29ac
EstefaniaMB <estefaniam707@gmail.com>
2017-03-08 05:43:12 -0500

    Primer Commit

---
+++ src/MainParcial_EstefaniaMontaña.java
@@ -0,0 +1,24 @@
+import processing.core.PApplet;
+
+public class MainParcial_EstefaniaMontaña extends PApplet {
+
+	public static void main(String[] args) {
+		PApplet.main("MainParcial_EstefaniaMontaña");
+	}
+
+	@Override
+	public void settings() {
+		size(1200, 700);
+	}
+
+	@Override
+	public void setup() {
+
+	}
+
+	@Override
+	public void draw() {
+		background(255);
+	}
+
+}
