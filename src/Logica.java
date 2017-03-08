a16193d7be9fe5ce96360a543593aeabb064b33e
EstefaniaMB <estefaniam707@gmail.com>
2017-03-08 08:35:21 -0500

    Cargar archivos txt y realizar sketch de la clase Persona

--- src/Logica.java
+++ src/Logica.java
@@ -19,14 +19,64 @@ public class Logica {
 		personaL = new LinkedList<Persona>();
 		personaH = new HashSet<Persona>();
 		personaT = new TreeSet<Persona>();
+		readText();
+
 	}
-	
-	public void pintar(){
-		
+
+	public void readText() {
+
+		// creo un arreglo de Strings en donde voy a almacenar las lineas del
+		// txt "usuarios"
+		String[] usuarios = app.loadStrings("../data/usuarios.txt");
+
+		// recorro mi arreglo de Strings
+		for (int i = 0; i < usuarios.length; i++) {
+			// voy a crear un nuevo arreglo de Strings en donde voy a guardar la
+			// información de "usuarios" separada por ":"
+			String[] datosU = usuarios[i].split(":");
+		}
+
+		// creo un arreglo de Strings en donde voy a almacenar las lineas del
+		// txt "información"
+		String[] informacion = app.loadStrings("../data/informacion.txt");
+		// recorro mi nuevo arreglo de Strings
+		for (int i = 0; i < informacion.length; i++) {
+			// nuevo arreglo de Strings en donde voy a almacenar la informacion
+			// de "informacion" separada por ":"
+			String[] datosI = informacion[i].split(":");
+			// ahora voy a guardar los datos separados en un nuevo arreglo
+			int[] datosInfo = new int[datosI.length];
+			for (int j = 0; j < datosI.length; j++) {
+				datosInfo[i] = Integer.parseInt(datosI[i]);
+			}
+		}
+
+		// creo un arreglo de Strings en donde voy a almacenar las lineas del
+		// archivo txt "tonos"
+
+		String[] tonos = app.loadStrings("../data/tonos.txt");
+
+		// recorro mi nuevo arreglo
+		for (int i = 0; i < tonos.length; i++) {
+			// creo un arreglo de Strings en donde almaceno los datos de "tonos"
+			// separados por ":"
+			String[] datosT = tonos[i].split(":");
+			// guardo los datos de "tono" separados en un nuevo arreglo,esta vez
+			// de int
+			int[] datosTono = new int[datosT.length];
+			for (int j = 0; j < datosT.length; j++) {
+				datosTono[i] = Integer.parseInt(datosT[i]);
+			}
+		}
+
 	}
-	
-	public void key(){
-		
+
+	public void pintar() {
+
+	}
+
+	public void key() {
+
 	}
 
 }
---
+++ src/Persona.java
@@ -0,0 +1,17 @@
+import processing.core.PApplet;
+
+public class Persona {
+
+	private PApplet app;
+	private String nombre, apellido;
+	private int cedula, edad, peso, r, g, b;
+
+	public Persona() {
+		this.app = app;
+	}
+	
+	public void pintar(){
+		
+	}
+
+}
